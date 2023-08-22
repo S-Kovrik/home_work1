84d18a431386284e65dab4c8718ee09671d46d50
Екатерина Солдатенко <sergey.covrik@gmail.com>
2023-08-22 15:03:58 +0300

    add_maket_21

---
+++ выпуск/radio/style/script.js
@@ -0,0 +1,7 @@
+function myFunctionHeader() {
+  document.getElementById("mydropdown__nav").classList.toggle("show");
+}
+
+function myFunctionFilter() {
+  document.getElementById("mydropdown__filter").classList.toggle("show");
+}
