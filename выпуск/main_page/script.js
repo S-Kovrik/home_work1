1cec5ae5fd9d2694e55358da40c5af81f862fada
Екатерина Солдатенко <sergey.covrik@gmail.com>
2023-08-23 19:51:43 +0300

    add_maket_23

--- выпуск/main_page/script.js
+++ выпуск/main_page/script.js
@@ -1,3 +1,15 @@
 function myFunctionHeader() {
-    document.getElementById("mydropdown__nav").classList.toggle("show");
-  }
+  document.getElementById("mydropdown__sublist").classList.toggle("show");
+  document.getElementById("mydropdown__svg").classList.toggle("svg_absolute");
+
+}
+
+$(document).ready(function () {
+  $(".open").on("click", function () {
+    $("#menu_nav").css("top", "0");
+  });
+
+  $(".close").on("click", function () {
+    $("#menu_nav").css("top", "-1000px");
+  });
+});
