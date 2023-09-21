08afaaed723483185f9663895da7b44d20d23a65
S-Kovrik <sergey.covrik@gmail.com>
2023-09-21 18:58:01 +0300

    fc

---
+++ FD2/in_classe/lesson1/index.js
@@ -0,0 +1,7 @@
+const FALLBACK_VALUE = "none";
+const firstName = prompt("Enter your name") || FALLBACK_VALUE;
+const lastName = prompt("Enter last name") || FALLBACK_VALUE;
+const middleName = prompt("Enter your middle name") || FALLBACK_VALUE;
+
+const fio = `${lastName} ${firstName} ${middleName}`;
+alert(fio);
